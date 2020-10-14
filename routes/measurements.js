6f33eccc960b70bde00ae9724b27027a38031366
GhostSD <ghostsoftwaredevelopment@gmail.com>
2020-10-14 09:21:15 +0200

    Update measurement route

--- routes/measurements.js
+++ routes/measurements.js
@@ -1,15 +1,29 @@
 const express = require( 'express' );
 const router = express.Router();
 const { ensureAuth } = require( '../middleware/auth' );
+const mongoose = require( 'mongoose' );
 
 const Examination = require( '../models/examination' );
-const Group = require( '../models/group' );
+const { Measurement, Result } = require( '../models/measurement' );
 
 // @desc    Measurements list
 // @route   GET /measurements
 router.get( '/', ensureAuth, async ( req, res ) => {
     try {
-        res.render( 'measurements' );
+        const lists = await Measurement
+            .find( { user: req.user.id } )
+            .sort( {
+                "createdAt": 1,
+            } )
+            .populate( 'examination' )
+            .populate( 'values' )
+            .populate( 'values.value' )
+            .lean();
+
+        console.log( lists[ 0 ] );
+        res.render( 'measurements', {
+            lists
+        } );
     } catch ( error ) {
         console.error( error );
         res.render( 'error/500' );
@@ -18,15 +32,35 @@ router.get( '/', ensureAuth, async ( req, res ) => {
 
 // @desc    Process add new measurement
 // @route   POST /measurements
+// @return  JSON data
 router.post( '/', ensureAuth, async ( req, res ) => {
-    req.body.user = req.user.id;
-    console.log( req.body );
+
+    let MeasurementBase = {
+        examination: req.body.examination,
+        user: req.user.id,
+        createdAt: Date.parse( `${req.body.date} ${req.body.time}` )
+    };
+
+    let values = new Array();
+    for ( let key in req.body ) {
+        if ( key.slice( 0, 6 ).toLowerCase() === 'value-' ) {
+            let valueID = key.slice( 6 );
+            values.push( {
+                value: new mongoose.Types.ObjectId( valueID ),
+                result: req.body[ key ]
+            } )
+        }
+    }
 
     try {
-        res.render( 'measurements' );
+        const result = await Measurement.create( {
+            values,
+            ...MeasurementBase
+        } );
+        res.json( result );
     } catch ( error ) {
         console.error( error );
-        res.render( 'error/500' );
+        res.json( { error } )
     }
 } );
 
