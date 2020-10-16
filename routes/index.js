3d5830800bb7a43f32095fd45afa2a65baa8699c
GhostSD <ghostsoftwaredevelopment@gmail.com>
2020-10-16 22:03:10 +0200

    add template route for static class TemplateFetch

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const path = require( "path" );
 const express = require( 'express' );
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require( '../middleware/auth' );
@@ -41,6 +42,30 @@ router.get( '/dashboard', ensureAuth, async ( req, res ) => {
         console.error( error );
         res.render( 'error/500' );
     }
+} );
+
+// @desc    Get template file
+// @route   GET /template/:id
+router.get( '/template/:name', ensureAuth, ( req, res, next ) => {
+    const options = {
+        root: path.join( __dirname, '../', 'views/partials' ),
+        dotfiles: 'deny',
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+    }
+
+    let fileName = req.params.name.split( '.' ).join( '/' );
+    console.log( fileName );
+
+    res.sendFile( fileName + '.hbs', options, function ( err ) {
+        if ( err ) {
+            res.sendStatus( 404 );
+        } else {
+            console.log( 'Sent:', fileName )
+        }
+    } )
 } );
 
 module.exports = router;
