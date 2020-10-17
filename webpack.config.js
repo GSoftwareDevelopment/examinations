058025a7e1b77b263cd15c19430e6c64d6ef590c
GhostSD <ghostsoftwaredevelopment@gmail.com>
2020-10-17 18:38:29 +0200

    Update files

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,45 @@
+const { parseTwoDigitYear } = require( 'moment' );
 const path = require( 'path' );
 const webpack = require( 'webpack' );
+
+const entryPath = "./src/js";
 
 module.exports = {
     mode: 'development',
     entry: {
-        main: './src/js/app.js',
-        examinations: './src/js/examinations.js',
-        measurements: './src/js/measurements.js',
+        app: path.resolve( __dirname, entryPath, 'app.js' ),
+        login: path.resolve( __dirname, entryPath, 'login.js' ),
+        dashboard: path.resolve( __dirname, entryPath, 'dashboard.js' ),
+        examinations: path.resolve( __dirname, entryPath, 'examinations.js' ),
+        measurements: path.resolve( __dirname, entryPath, 'measurements.js' ),
     },
     output: {
         path: __dirname + '/dist',
         filename: '[name].bundle.js',
     },
-    plugins: [
-        new webpack.ProvidePlugin( { $: 'jquery', jQuery: 'jquery' } ),
-    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                enforce: 'pre',
+                use: [ 'source-map-loader' ],
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ],
+            },
+            {
+                test: /\.hbs$/,
+                loader: 'handlebars-loader',
+                options: {
+                    runtime: path.resolve( __dirname, './src/js/helpers/handlebars-helpers.js' )
+                }
+            }
+        ],
+    },
+    // plugins: [
+    //     new webpack.ProvidePlugin( { $: 'jquery', jQuery: 'jquery' } ),
+    // ],
     watch: true,
     watchOptions: {
         ignored: [ 'node_modules/**' ]
