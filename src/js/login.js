c282ce2ffb1b52ef8c18f0c23434c54a2a746742
GhostSD <ghostsoftwaredevelopment@gmail.com>
2020-10-17 18:36:26 +0200

    Entry point for login page

---
+++ src/js/login.js
@@ -0,0 +1,13 @@
+// import $ from "jquery";
+import './../scss/progressbar.scss';
+import './../scss/style.scss';
+
+$( 'button' ).click( e => {
+    $( 'button' ).addClass( 'disabled' );
+    let target = $( e.currentTarget ),
+        href = target.data( 'href' );
+
+    $( '.progress' ).show();
+
+    if ( href ) window.location = href;
+} )
