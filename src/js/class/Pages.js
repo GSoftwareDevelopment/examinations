f2f150900e85d523abec75b16e1ff65978aa4fa6
GhostSD <ghostsoftwaredevelopment@gmail.com>
2020-10-18 14:28:13 +0200

    Update file

--- src/js/class/Pages.js
+++ src/js/class/Pages.js
@@ -1,7 +1,17 @@
 export class Pages {
     constructor( path ) {
-        this.modal = {};
+        this.page = $( `main[role='main']` );
         this.path = path;
+
+        this.modal = {};
+
+        let _progress = $( this.page ).find( '[role="progressbar"], [role="status"]' ).get();
+        this.progress = new Array();
+
+        for ( let el of _progress ) {
+            this.progress[ el.id ] = el;
+            $( el ).hide();
+        }
 
         const isPath = path === window.location.pathname
         return isPath;
