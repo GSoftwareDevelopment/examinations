542d73f6d3be09929d8719ab07c7a4a02efb6e50
GhostSD <ghostsoftwaredevelopment@gmail.com>
2020-10-28 13:49:36 +0100

    correct typo and bad route

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,11 +1,10 @@
-const path = require( "path" );
 const express = require( 'express' );
 const router = express.Router();
-const { ensureAuth, ensureGuest } = require( '../middleware/auth' );
+const { ensureAuth } = require( '../middleware/auth' );
 
 // @desc    Dashboard
 // @route   GET /dashboard
-router.get( '/dashboard', ensureAuth, async ( req, res ) => {
+router.get( '/', ensureAuth, async ( req, res ) => {
     try {
         res.render( 'dashboard', {
             name: req.user.firstName,
