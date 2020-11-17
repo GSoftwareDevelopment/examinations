af5857ed9813a3ebbf45996fae67ba35c5592538
GhostSD <ghostsoftwaredevelopment@gmail.com>
2020-11-17 18:02:31 +0100

    refactoring to variable export

--- frontend-react/src/api-routes.js
+++ frontend-react/src/api-routes.js
@@ -3,16 +3,17 @@ const BASE_URL = "http://192.168.0.234:3000";
 const API_URL = BASE_URL + "/api";
 const AUTH_URL = BASE_URL + "/auth";
 
-export default {
-    API_URL,
-    authenticate: AUTH_URL + "/login",
-    registerUser: AUTH_URL + "/register",
-    userData: API_URL + "/user",
-    examinations: API_URL + "/examinations",
-    groups: API_URL + "/groups",
-    measurementList: API_URL + "/measurements",
-    measurementLatest: API_URL + "/measurements/latest",
-    valuesList: API_URL + "/values",
+const ROUTES = {
+	API_URL,
+	authenticate: AUTH_URL + "/login",
+	registerUser: AUTH_URL + "/register",
+	userData: API_URL + "/user",
+	examinations: API_URL + "/examinations",
+	groups: API_URL + "/groups",
+	measurementList: API_URL + "/measurements",
+	measurementLatest: API_URL + "/measurements/latest",
+	valuesList: API_URL + "/values",
 
-    confExaminations: API_URL + '/examinations/configuration',
+	confExaminations: API_URL + '/examinations/configuration',
 }
+export default ROUTES
