51281d7ddd6d34c98f9d5408457d3038503db76d
GhostSD <ghostsoftwaredevelopment@gmail.com>
2020-11-18 18:22:46 +0100

    added functionality: value retrieval via backend, state and error handling.

--- frontend-react/src/stores/values.js
+++ frontend-react/src/stores/values.js
@@ -1,10 +1,23 @@
+import API from '../api-routes';
 import { makeAutoObservable, runInAction } from 'mobx';
+import UserStore from '../stores/user';
 
 class ValuesStore {
+	state = "done" // "pending" / "error" / "done"
 	items = []
+	error = null;
 
 	constructor() {
 		makeAutoObservable( this );
+	}
+
+	getState () { return this.state }
+	getError () { return this.error }
+	clearError () {
+		runInAction( () => {
+			this.state = 'done';
+			this.error = null;
+		} )
 	}
 
 	reset ( defaults ) {
@@ -12,6 +25,67 @@ class ValuesStore {
 	}
 
 	getItems () { return this.items; }
+
+	async fetchGet ( examinationId ) {
+		this.state = "pending";
+		// this.items = [];
+
+		if ( UserStore.state !== 'logged' ) {
+			console.log( `Can't do operation 'fetchGet' when user is not logged` );
+			return;
+		}
+		const token = UserStore.getToken();
+
+		try {
+
+			const res = await fetch( API.valuesList + `/${examinationId}`,
+				{
+					method: 'GET',
+					headers: {
+						'Content-Type': 'application/json',
+						'Authorization': 'Bearer ' + token
+					}
+				} );
+
+			let result = await res.json();
+
+			runInAction( () => {
+				if ( result.OK ) {
+					this.items = result.values.map( value => {
+						// eslint-disable-next-line
+						const { createdAt, examination, name, required, type, user, __v, _id, ...rest } = value;
+						return {
+							id: _id,
+							type,
+							name,
+							required,
+							...rest
+						}
+					} );
+					this.state = "done";
+				} else {
+					console.error( 'Backend error', result );
+					this.state = "error";
+					this.error = {
+						title: 'Backend error',
+						msg: result.error.message,
+						error: result.error
+					}
+				}
+			} );
+
+		} catch ( error ) {
+			runInAction( () => {
+				console.error( 'Fetch error:', error );
+				this.state = "error";
+				this.error = {
+					title: 'Fetch error',
+					msg: error.message,
+					error
+				}
+			} )
+		}
+	}
 
 	insert ( data ) {
 		console.log( 'Adding new value...', data )
