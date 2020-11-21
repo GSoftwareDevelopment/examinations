dd04765defeb1159aaf0d9a7967e3d16cda0c828
GhostSD <ghostsoftwaredevelopment@gmail.com>
2020-11-21 16:28:35 +0100

    Added method to update group data. Prettier in action.

--- frontend-react/src/stores/groups.js
+++ frontend-react/src/stores/groups.js
@@ -1,74 +1,115 @@
-import API from '../api-routes';
-import { makeObservable, observable, action, runInAction } from 'mobx';
-import Fetcher from './Fetcher';
-import ExaminationsStore from '../stores/examinations';
+import API from "../api-routes";
+import { makeObservable, observable, action, runInAction } from "mobx";
+import Fetcher from "./Fetcher";
+import ExaminationsStore from "../stores/examinations";
 
 class GroupsStore extends Fetcher {
 	items = [];
 
 	constructor() {
 		super(`Groups`);
-		makeObservable( this, {
+		makeObservable(this, {
 			items: observable,
 			getItems: action,
 			fetchGet: action,
 			fetchAdd: action,
 			fetchDelete: action,
 			insert: action,
+			update: action,
 			remove: action,
-		} );
+		});
 	}
 
-	getItems () {
+	getItems() {
 		return this.items;
 	}
-
-	async fetchGet () {
-		let result = await this.fetch( API.groups, 'GET' );
-		runInAction( () => {
-			if ( result.OK ) {
+	getByID(groupId) {
+		return this.items.find((item) => item._id === groupId);
+	}
+	async fetchGet() {
+		let result = await this.fetch(API.groupsGet, "GET");
+		runInAction(() => {
+			if (result.OK) {
 				this.items = result.groups;
 				this.state = "done";
 			}
-		} );
+		});
 	}
 
-	async fetchAdd ( newGroup ) {
-		const result = await this.fetch( API.groups, 'POST', JSON.stringify( newGroup ) );
-		runInAction( () => {
-			if ( result.OK ) {
-				this.insert( result.created );
+	async fetchAdd(newGroup) {
+		const result = await this.fetch(
+			API.groupsCreate,
+			"POST",
+			JSON.stringify(newGroup)
+		);
+		runInAction(() => {
+			if (result.OK) {
+				this.insert(result.created);
 				this.state = "done";
 			}
-		} );
+		});
 
 		return result;
 	}
 
-	async fetchDelete ( groupsIDsList ) {
-		const result = await this.fetch( API.groups, 'DELETE', JSON.stringify( groupsIDsList ) )
+	async fetchUpdate(groupId, groupData) {
+		const result = await this.fetch(
+			API.groupsUpdate + groupId,
+			"POST",
+			JSON.stringify(groupData)
+		);
 
-		runInAction( () => {
-			if ( result.OK ) {
+		runInAction(() => {
+			if (result.OK) {
 				this.state = "done";
-				groupsIDsList.forEach( groupId => {
-					this.remove( groupId );
-				} );
+				this.update(groupId, groupData);
 			}
-		} );
+		});
 
 		return result;
 	}
 
-	insert ( group ) {
-		this.items.push( group );
+	async fetchDelete(groupsIDsList) {
+		const result = await this.fetch(
+			API.groupsDelete,
+			"DELETE",
+			JSON.stringify(groupsIDsList)
+		);
+
+		runInAction(() => {
+			if (result.OK) {
+				this.state = "done";
+				groupsIDsList.forEach((groupId) => {
+					this.remove(groupId);
+				});
+			}
+		});
+
+		return result;
 	}
 
-	remove ( groupId ) {
-		this.items = this.items.filter( group => ( group._id !== groupId ) );
-		ExaminationsStore.removeGroup( groupId );
+	insert(group) {
+		this.items.push(group);
+	}
+
+	update(groupId, groupData) {
+		runInAction(() => {
+			this.items.forEach((group, index) => {
+				if (group._id !== groupId) return;
+
+				this.items[index] = {
+					...group,
+					...groupData,
+				};
+			});
+		});
+	}
+
+	remove(groupId) {
+		this.items = this.items.filter((group) => group._id !== groupId);
+		ExaminationsStore.removeGroup(groupId);
 	}
 }
 
-let groupStore = window.groupStore = new GroupsStore();
-export default groupStore;
+let groupsStore = (window.groupsStore = new GroupsStore());
+export default groupsStore;
