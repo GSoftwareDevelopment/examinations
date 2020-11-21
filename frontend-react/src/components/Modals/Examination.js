e2982fb845b3345d5308f8dea027a36f52d10758
GhostSD <ghostsoftwaredevelopment@gmail.com>
2020-11-21 08:20:15 +0100

    BUGFIX: Creating a new group while editing a examination did not transfer the new state to the parent component. CHANGE: changed the way of passing state to the parent component. Not through an event, but an object with content: `name` - the name of the parent state property and its `value`

--- frontend-react/src/components/Modals/Examination.js
+++ frontend-react/src/components/Modals/Examination.js
@@ -1,54 +1,60 @@
-import React, { Component } from 'react';
-import { observer } from 'mobx-react';
-import './examination.scss';
+import React, { Component } from "react";
+import { observer } from "mobx-react";
+import "./examination.scss";
 
-import ExaminationsStore from '../../stores/examinations';
-import ValuesStore from '../../stores/values';
-import ValidationStore from '../../stores/validation';
+import ExaminationsStore from "../../stores/examinations";
+import ValuesStore from "../../stores/values";
+import ValidationStore from "../../stores/validation";
 
-import { Form, Button, Modal, Tabs, Tab, Spinner } from 'react-bootstrap';
-import * as Icon from 'react-bootstrap-icons';
-import { ModalTabsButtons } from '../ModalTabsButtons';
+import { Form, Button, Modal, Tabs, Tab, Spinner } from "react-bootstrap";
+import * as Icon from "react-bootstrap-icons";
+import { ModalTabsButtons } from "../ModalTabsButtons";
 
-import TabGeneral from './examination-tabs/General';
-import TabValues from './examination-tabs/Values';
+import TabGeneral from "./examination-tabs/General";
+import TabValues from "./examination-tabs/Values";
 
 class ModalExamination extends Component {
-	constructor( props ) {
-		super( props );
+	constructor(props) {
+		super(props);
 
-		if ( !props.setTo ) {
+		if (!props.setTo) {
 			// if `setTo` property is set to `null` it indicates creating a new examination
 			this.state = {
-				activeTab: 'general',
-				name: 'Nowe badanie',
+				activeTab: "general",
+				name: "Nowe badanie",
 				group: null,
-				description: '',
-			}
+				description: "",
+			};
 			ValuesStore.reset();
-			ValuesStore.insert( { type: 'numeric', name: 'Wartość', description: '', required: true, unit: '' } );
+			ValuesStore.insert({
+				type: "numeric",
+				name: "Wartość",
+				description: "",
+				required: true,
+				unit: "",
+			});
 		} else {
 			// ...otherwise, it is edit mode of existing examination.
 			// Inside `isSet` property is object with examination data
 			this.state = {
-				activeTab: 'general',
-				...props.setTo
-			}
+				activeTab: "general",
+				...props.setTo,
+			};
 		}
 
-		ValidationStore.removeField( 'modal-examination' );
-		ValidationStore.setField( 'modal-examination', 'values', true );
+		ValidationStore.removeField("modal-examination");
+		ValidationStore.setField("modal-examination", "values", true);
 	}
 
-	setInputValue ( property, val ) {
-		this.setState( oldState => {
-			if ( property === 'group' && val === '' ) val = null;
-			let changes = { [ property ]: val }
-			return { ...oldState, ...changes }
-		} );
+	setInputValue(property, val) {
+		this.setState((oldState) => {
+			if (property === "group" && val === "") val = null;
+			let changes = { [property]: val };
+			return { ...oldState, ...changes };
+		});
 	}
 
-	async doSubmit ( e ) {
+	async doSubmit(e) {
 		e.preventDefault();
 		e.stopPropagation();
 
@@ -58,99 +64,125 @@ class ModalExamination extends Component {
 			group: this.state.group || null,
 			description: this.state.description,
 			// TODO: Po stronie backendu, przy tworzeniu badania, wyrzucić ID z listy wartości przed ich utworzeniem
-			values: ValuesStore.getItems()
-		}
+			values: ValuesStore.getItems(),
+		};
 
-		if ( !this.props.setTo )
-			result = await ExaminationsStore.fetchAdd( examinationData )
+		if (!this.props.setTo)
+			result = await ExaminationsStore.fetchAdd(examinationData);
 		else
-			result = await ExaminationsStore.fetchUpdate( this.state._id, examinationData )
+			result = await ExaminationsStore.fetchUpdate(
+				this.state._id,
+				examinationData
+			);
 
-		if ( result.OK ) {
+		if (result.OK) {
 			this.props.onHide();
-			return
+			return;
 		}
 	}
 
-	isValid () {
-		return ValidationStore.check( 'modal-examination' );
+	isValid() {
+		return ValidationStore.check("modal-examination");
 	}
 
-	render () {
-
+	render() {
 		const tabs = [
 			{
-				key: 'general',
-				name: 'Ogólne',
-				afterName: <React.Fragment>
-					{ValidationStore.check( 'modal-examination', 'name' ) === false &&
-						<Icon.ExclamationDiamond size="16" className="ml-1 text-danger" />}
-				</React.Fragment>
+				key: "general",
+				name: "Ogólne",
+				afterName: (
+					<React.Fragment>
+						{ValidationStore.check("modal-examination", "name") === false && (
+							<Icon.ExclamationDiamond size="16" className="ml-1 text-danger" />
+						)}
+					</React.Fragment>
+				),
 			},
 			{
-				key: 'values',
-				name: 'Wartości',
-				afterName: <React.Fragment>
-					{ValidationStore.check( 'modal-examination', 'values' ) === false &&
-						<Icon.ExclamationDiamond size="16" className="ml-1 text-danger" />}
-				</React.Fragment>
+				key: "values",
+				name: "Wartości",
+				afterName: (
+					<React.Fragment>
+						{ValidationStore.check("modal-examination", "values") === false && (
+							<Icon.ExclamationDiamond size="16" className="ml-1 text-danger" />
+						)}
+					</React.Fragment>
+				),
 			},
 			{
-				key: 'norms',
-				name: 'Normy',
-			}
-		]
+				key: "norms",
+				name: "Normy",
+			},
+		];
 
 		return (
-			<Modal show={this.props.show} onHide={this.props.onHide} backdrop="static" autoFocus={false}>
+			<Modal
+				show={this.props.show}
+				onHide={this.props.onHide}
+				backdrop="static"
+				autoFocus={false}
+			>
 				<Form
 					autoComplete="off"
-					onSubmit={( e ) => { this.doSubmit( e ) }}
+					onSubmit={(e) => {
+						this.doSubmit(e);
+					}}
 				>
 					<Modal.Header closeButton className="pb-0 border-0">
-						{!this.props.setTo
-							? <Modal.Title>Nowe badanie</Modal.Title>
-							: <Modal.Title>Edycja badania</Modal.Title>
-						}
+						{!this.props.setTo ? (
+							<Modal.Title>Nowe badanie</Modal.Title>
+						) : (
+							<Modal.Title>Edycja badania</Modal.Title>
+						)}
 					</Modal.Header>
 
 					<ModalTabsButtons
 						activeTab={this.state.activeTab}
 						tabs={tabs}
-						onTabSelect={( key ) => { this.setState( { activeTab: key } ); }} />
+						onTabSelect={(key) => {
+							this.setState({ activeTab: key });
+						}}
+					/>
 
 					<Modal.Body>
-						<Tabs
-							className="border-0"
-							activeKey={this.state.activeTab}>
+						<Tabs className="border-0" activeKey={this.state.activeTab}>
 							<Tab eventKey="general">
 								<TabGeneral
 									setTo={this.props.setTo}
-									onChange={( e ) => { this.setInputValue( e.target.name, e.target.value ) }}
+									onChange={(state) => {
+										this.setInputValue(state.name, state.value);
+									}}
 								/>
 							</Tab>
 							<Tab eventKey="values">
 								<TabValues />
 							</Tab>
-							<Tab eventKey="norms">
-							</Tab>
+							<Tab eventKey="norms"></Tab>
 						</Tabs>
 					</Modal.Body>
 					<Modal.Footer>
 						<Button
-							disabled={!this.isValid() || !ExaminationsStore.getState() === 'pending'}
-							type="submit">
-							{ExaminationsStore.getState() === 'pending'
-								? <div className="d-flex felx-row align-items-center">
-									<Spinner size="sm" animation="border" role="status" /><span className="ml-2">Zapisywanie...</span>
+							disabled={
+								!this.isValid() || !ExaminationsStore.getState() === "pending"
+							}
+							type="submit"
+						>
+							{ExaminationsStore.getState() === "pending" ? (
+								<div className="d-flex felx-row align-items-center">
+									<Spinner size="sm" animation="border" role="status" />
+									<span className="ml-2">Zapisywanie...</span>
 								</div>
-								: this.props.setTo ? "Zapisz" : "Dodaj"}
+							) : this.props.setTo ? (
+								"Zapisz"
+							) : (
+								"Dodaj"
+							)}
 						</Button>
 					</Modal.Footer>
 				</Form>
-			</Modal >
-		)
+			</Modal>
+		);
 	}
 }
 
-export default observer( ModalExamination )
+export default observer(ModalExamination);
--- frontend-react/src/components/Modals/examination-tabs/General.js
+++ frontend-react/src/components/Modals/examination-tabs/General.js
@@ -1,133 +1,161 @@
-import React, { Component } from 'react'
-import { observer } from 'mobx-react';
+import React, { Component } from "react";
+import { observer } from "mobx-react";
 
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button } from "react-bootstrap";
 
-import ExaminationsStore from '../../../stores/examinations';
-import GroupsStore from '../../../stores/groups';
-import ValidationStore from '../../../stores/validation';
+import ExaminationsStore from "../../../stores/examinations";
+import GroupsStore from "../../../stores/groups";
+import ValidationStore from "../../../stores/validation";
 
-import AddGroup from '../AddGroup';
+import AddGroup from "../AddGroup";
 
 class TabGeneral extends Component {
+	constructor(props) {
+		super(props);
 
-	constructor( props ) {
-		super( props );
-
-		if ( !props.setTo )
+		if (!props.setTo)
 			this.state = {
-				name: '',
+				name: "",
 				group: null,
-				description: '',
+				description: "",
 				modalAddGroup: false,
-			}
+			};
 		else
 			this.state = {
 				id: props.setTo._id,
 				name: props.setTo.name,
 				group: props.setTo.group || null,
 				description: props.setTo.description,
-			}
+			};
 		this.innerRef = React.createRef();
 	}
 
-	componentDidMount () {
-		setTimeout( () => {
+	componentDidMount() {
+		setTimeout(() => {
 			this.innerRef.current.focus();
-		}, 1 )
+		}, 1);
 	}
 
-	validate () {
-
+	validate() {
 		let { id: _id, name, group } = this.state;
 		name = name.trim();
 
-		if ( name === '' ) {
-			ValidationStore.setField( 'modal-examination', 'name', 'Wprowadź Nazwę badania' )
-		} else if ( ExaminationsStore.getItems()
-			.find( item => item._id !== _id && item.group === group && item.name === name )
+		if (name === "") {
+			ValidationStore.setField(
+				"modal-examination",
+				"name",
+				"Wprowadź Nazwę badania"
+			);
+		} else if (
+			ExaminationsStore.getItems().find(
+				(item) => item._id !== _id && item.group === group && item.name === name
+			)
 		) {
-			ValidationStore.setField( 'modal-examination', 'name', 'W przypisanej grupie, podana nazwa badania juz występuje.' )
+			ValidationStore.setField(
+				"modal-examination",
+				"name",
+				"W przypisanej grupie, podana nazwa badania juz występuje."
+			);
 		} else {
-			ValidationStore.setField( 'modal-examination', 'name', true );
+			ValidationStore.setField("modal-examination", "name", true);
 		}
 	}
 
-	render () {
-		return ( <React.Fragment>
-			<Form.Group controlId="name" className="mt-3">
-				<Form.Label>Nazwa badania</Form.Label>
-				<Form.Control
-					ref={this.innerRef}
-					type="text"
-					name={"name"}
-					required
-					value={this.state.name}
-					autoFocus
-					onChange={( e ) => {
-						const value = e.target.value;
-						this.setState( { name: value } );
-						this.props.onChange( e );
-					}}
-					onBlur={( e ) => { this.validate(); }}
-				/>
-				{ValidationStore.formMessage( 'modal-examination', 'name' )}
-			</Form.Group>
-			<Form.Group>
-				<div className="d-flex flex-row justify-content-between align-items-center">
-					<Form.Label htmlFor="group">Przypisz do grupy</Form.Label>
-					<Button
-						variant="light"
-						size="sm"
-						onClick={() => { this.setState( { modalAddGroup: true } ); }}
-					>Utwórz grupę</Button>
-				</div>
-				<Form.Control
-					as="select"
-					custom
-					value={this.state.group || ""}
-					name={"group"}
-					onChange={( e ) => {
-						this.setState( { group: e.target.value || null } );
-						this.forceUpdate( () => {
+	render() {
+		return (
+			<React.Fragment>
+				<Form.Group controlId="name" className="mt-3">
+					<Form.Label>Nazwa badania</Form.Label>
+					<Form.Control
+						ref={this.innerRef}
+						type="text"
+						name={"name"}
+						required
+						value={this.state.name}
+						autoFocus
+						onChange={(e) => {
+							const { name, value } = e.target;
+							this.setState({ name: value });
+							this.props.onChange({ name, value });
+						}}
+						onBlur={(e) => {
 							this.validate();
-						} );
-						this.props.onChange( e );
-					}}
-				>
-					{GroupsStore.items.length > 0
-						? <option key="noGroup" value="">Nie przypisuj do żadnej grupy</option>
-						: <option key="empty" value="" disabled={true}>Brak zdefiniowanych grup</option>
-					}
-					{GroupsStore.items.map( group =>
-						<option key={group._id} value={group._id}>{group.name}</option>
-					)}
-				</Form.Control>
-			</Form.Group>
-			<Form.Group className="form-group">
-				<Form.Label htmlFor="description">Opis</Form.Label>
-				<Form.Control
-					as="textarea"
-					rows="3"
-					name={"description"}
-					value={this.state.description || ""}
-					onChange={( e ) => {
-						const value = e.target.value;
-						this.setState( { description: value } );
-						this.props.onChange( e );
-					}}
-				/>
-			</Form.Group>
-			{
-				this.state.modalAddGroup &&
-				<AddGroup
-					show={this.state.modalAddGroup}
-					onHide={() => { this.setState( { modalAddGroup: false } ); }}
-					isCreated={( group ) => { this.setState( { group: group._id } ) }}
-				/>
-			}
-		</React.Fragment> )
+						}}
+					/>
+					{ValidationStore.formMessage("modal-examination", "name")}
+				</Form.Group>
+				<Form.Group>
+					<div className="d-flex flex-row justify-content-between align-items-center">
+						<Form.Label htmlFor="group">Przypisz do grupy</Form.Label>
+						<Button
+							variant="light"
+							size="sm"
+							onClick={() => {
+								this.setState({ modalAddGroup: true });
+							}}
+						>
+							Utwórz grupę
+						</Button>
+					</div>
+					<Form.Control
+						as="select"
+						custom
+						value={this.state.group || ""}
+						name={"group"}
+						onChange={(e) => {
+							const { name, value } = e.target;
+							this.setState({ group: value || null });
+							this.forceUpdate(() => {
+								this.validate();
+							});
+							this.props.onChange({ name, value });
+						}}
+					>
+						{GroupsStore.items.length > 0 ? (
+							<option key="noGroup" value="">
+								Nie przypisuj do żadnej grupy
+							</option>
+						) : (
+							<option key="empty" value="" disabled={true}>
+								Brak zdefiniowanych grup
+							</option>
+						)}
+						{GroupsStore.items.map((group) => (
+							<option key={group._id} value={group._id}>
+								{group.name}
+							</option>
+						))}
+					</Form.Control>
+				</Form.Group>
+				<Form.Group className="form-group">
+					<Form.Label htmlFor="description">Opis</Form.Label>
+					<Form.Control
+						as="textarea"
+						rows="3"
+						name={"description"}
+						value={this.state.description || ""}
+						onChange={(e) => {
+							const { name, value } = e.target;
+							this.setState({ description: value });
+							this.props.onChange({ name, value });
+						}}
+					/>
+				</Form.Group>
+				{this.state.modalAddGroup && (
+					<AddGroup
+						show={this.state.modalAddGroup}
+						onHide={() => {
+							this.setState({ modalAddGroup: false });
+						}}
+						isCreated={(group) => {
+							this.setState({ group: group._id });
+							this.props.onChange({ name: "group", value: group._id });
+						}}
+					/>
+				)}
+			</React.Fragment>
+		);
 	}
 }
 
-export default observer( TabGeneral )
+export default observer(TabGeneral);
