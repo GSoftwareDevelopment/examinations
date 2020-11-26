5d49f57fafd58e6bb1ed2017d9f54f6f98749b81
GhostSD <ghostsoftwaredevelopment@gmail.com>
2020-11-26 15:14:59 +0100

    BUGFIX: in `formMessage` method, message filtering and condition for result.

--- frontend-react/src/stores/validation.js
+++ frontend-react/src/stores/validation.js
@@ -1,70 +1,76 @@
-import { makeAutoObservable } from 'mobx';
+import { makeAutoObservable } from "mobx";
 // import { autorun } from 'mobx';
-import { Form } from 'react-bootstrap';
+import { Form } from "react-bootstrap";
 
 class ValidationStore {
 	fields = [];
 
 	constructor() {
-		makeAutoObservable( this );
+		makeAutoObservable(this);
 	}
 
-	setField ( componentName, fieldName, value ) {
+	setField(componentName, fieldName, value) {
 		let found = false;
-		for ( let field of this.fields ) {
-			if ( field.component === componentName && field.name === fieldName ) {
+		for (let field of this.fields) {
+			if (field.component === componentName && field.name === fieldName) {
 				field.value = value;
-				found = true; break;
+				found = true;
+				break;
 			}
 		}
-		if ( found ) return;
-		this.fields.push( {
+		if (found) return;
+		this.fields.push({
 			component: componentName,
 			name: fieldName,
 			value,
-		} );
+		});
 	}
 
-	getField ( componentName, fieldName ) {
-		let componentFields = this.fields.filter( field => field.component === componentName );
-		if ( fieldName ) {
-			let fields = componentFields.filter( item => item.name === fieldName );
+	getField(componentName, fieldName) {
+		let componentFields = this.fields.filter((field) => field.component === componentName);
+		if (fieldName) {
+			let fields = componentFields.filter((item) => item.name === fieldName);
 			return fields;
-		} else
-			return componentFields;
+		} else return componentFields;
 	}
 
-	removeField ( componentName, fieldName ) {
+	removeField(componentName, fieldName) {
 		let newFields;
-		if ( fieldName )
-			newFields = this.fields.filter( field => !( field.component === componentName && field.name === fieldName ) )
-		else
-			newFields = this.fields.filter( field => !( field.component === componentName ) )
+		if (fieldName)
+			newFields = this.fields.filter(
+				(field) => !(field.component === componentName && field.name === fieldName)
+			);
+		else newFields = this.fields.filter((field) => !(field.component === componentName));
 
 		this.fields = newFields;
 	}
 
-	check ( componentName, fieldName ) {
+	check(componentName, fieldName) {
 		let valid = true;
-		this.getField( componentName, fieldName ).forEach( item => {
-			if ( item.value !== true ) {
+		this.getField(componentName, fieldName).forEach((item) => {
+			if (item.value !== true) {
 				valid = false;
-				return false
+				return false;
 			}
-		} );
+		});
 		return valid;
 	}
 
-	formMessage ( componentName, fieldName ) {
-		const invalidValues = this.getField( componentName, fieldName )
-			.filter( item => ( item !== false && item !== true ) );
-
-		return invalidValues.map( value =>
-			<Form.Text key={value.value} className="text-danger">{value.value}</Form.Text> )
+	formMessage(componentName, fieldName) {
+		const invalidValues = this.getField(componentName, fieldName).filter(
+			(item) => item.value !== false && item.value !== true
+		);
+		if (invalidValues.length > 0)
+			return invalidValues.map((value) => (
+				<Form.Text key={value.value} className="text-danger">
+					{value.value}
+				</Form.Text>
+			));
+		else return null;
 	}
 }
 
-let validationStore = window.validationStore = new ValidationStore();
+let validationStore = (window.validationStore = new ValidationStore());
 
 // autorun( () => {
 // 	console.log( '--- Validation store:' )
@@ -73,4 +79,4 @@ let validationStore = window.validationStore = new ValidationStore();
 // 	} )
 // } );
 
-export default validationStore
+export default validationStore;
