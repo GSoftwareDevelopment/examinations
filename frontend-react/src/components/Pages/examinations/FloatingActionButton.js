bca048d6e85a556c528f7afb8b4020c62e123cbb
GhostSD <ghostsoftwaredevelopment@gmail.com>
2020-11-26 08:48:20 +0100

    FIX: If dropdown is not defined, don't render dropdown component

--- frontend-react/src/components/Pages/examinations/FloatingActionButton.js
+++ frontend-react/src/components/Pages/examinations/FloatingActionButton.js
@@ -14,40 +14,42 @@ export default class FloatingActionButton extends Component {
 					{this.props.mainActionContent}
 				</Button>
 
-				<Dropdown drop="up">
-					<Dropdown.Toggle
-						variant="light"
-						id="dropdown-basic"
-						className={this.props.dropdown.trigger.className}
-						style={this.props.dropdown.trigger.style}
-					>
-						{this.props.dropdown.trigger.content}
-					</Dropdown.Toggle>
+				{this.props.dropdown && (
+					<Dropdown drop="up">
+						<Dropdown.Toggle
+							variant="light"
+							id="dropdown-basic"
+							className={this.props.dropdown.trigger.className}
+							style={this.props.dropdown.trigger.style}
+						>
+							{this.props.dropdown.trigger.content}
+						</Dropdown.Toggle>
 
-					<Dropdown.Menu>
-						{this.props.dropdown.header && (
-							<Dropdown.Header>{this.props.dropdown.header}</Dropdown.Header>
-						)}
-						{this.props.dropdown.items.map((item, index) => {
-							if (item) {
-								return (
-									<Dropdown.Item
-										key={index}
-										className={item.className}
-										disabled={item.disabled}
-										onClick={() => {
-											item.onClick();
-										}}
-									>
-										{item.icon} {item.text}
-									</Dropdown.Item>
-								);
-							} else {
-								return <Dropdown.Divider />;
-							}
-						})}
-					</Dropdown.Menu>
-				</Dropdown>
+						<Dropdown.Menu>
+							{this.props.dropdown.header && (
+								<Dropdown.Header>{this.props.dropdown.header}</Dropdown.Header>
+							)}
+							{this.props.dropdown.items.map((item, index) => {
+								if (item) {
+									return (
+										<Dropdown.Item
+											key={index}
+											className={item.className}
+											disabled={item.disabled}
+											onClick={() => {
+												item.onClick();
+											}}
+										>
+											{item.icon} {item.text}
+										</Dropdown.Item>
+									);
+								} else {
+									return <Dropdown.Divider />;
+								}
+							})}
+						</Dropdown.Menu>
+					</Dropdown>
+				)}
 			</nav>
 		);
 	}
