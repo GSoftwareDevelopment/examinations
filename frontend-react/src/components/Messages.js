a6970f7bb0f9b6da1ed659a4dc3126b30686837f
GhostSD <ghostsoftwaredevelopment@gmail.com>
2020-11-26 15:12:56 +0100

    Message library

---
+++ frontend-react/src/components/Messages.js
@@ -0,0 +1,9 @@
+export const messagesNoElements = <div>Brak elementów</div>;
+export const messageNoExaminationsAndGroups = <div>Brak zdefiniowanych badań i grup.</div>;
+export const messageGroupHasntExaminations = <div>Ta grupa nie posiada żadnych badań</div>;
+export const messageChoiceGroup = (
+	<div className="mt-4">
+		Wybierz element z <strong>Listy grup</strong>, aby zobaczyć badania przypisane do tej grupy.
+	</div>
+);
+export const messageNoValuesDefinitions = <div>Brak definicji wartości określających badanie.</div>;
