9f47e6d685b8a001bdb905d9ab3beedf42ddf4a8
GhostSD <ghostsoftwaredevelopment@gmail.com>
2020-11-30 10:30:30 +0100

    Messages as components.

--- frontend-react/src/components/Messages.js
+++ frontend-react/src/components/Messages.js
@@ -1,9 +1,34 @@
-export const messagesNoElements = <div>Brak elementów</div>;
-export const messageNoExaminationsAndGroups = <div>Brak zdefiniowanych badań i grup.</div>;
-export const messageGroupHasntExaminations = <div>Ta grupa nie posiada żadnych badań</div>;
-export const messageChoiceGroup = (
-	<div className="mt-4">
-		Wybierz element z <strong>Listy grup</strong>, aby zobaczyć badania przypisane do tej grupy.
-	</div>
+import * as Icon from "react-bootstrap-icons";
+import InfoBox from "./Layout/InfoBox";
+
+export const messagesNoElements = <InfoBox content={<div>Brak elementów</div>} />;
+
+export const messageNoExaminationsAndGroups = (
+	<InfoBox
+		key={`info-noItems`}
+		icon={null}
+		content={<div>Brak zdefiniowanych badań i grup.</div>}
+	/>
 );
-export const messageNoValuesDefinitions = <div>Brak definicji wartości określających badanie.</div>;
+
+export const messageGroupHasntExaminations = (
+	<InfoBox key={`info-emptyGroup`} icon={null} content={<div>Ta grupa nie posiada badań</div>} />
+);
+
+export const messageChoiceGroup = (
+	<InfoBox
+		icon={<Icon.InfoSquare size="64" />}
+		content={
+			<div className="mt-4">
+				Wybierz element z <strong>Listy grup</strong>, aby zobaczyć badania przypisane do tej grupy.
+			</div>
+		}
+	/>
+);
+
+export const messageNoValuesDefinitions = (
+	<InfoBox
+		icon={<Icon.EmojiDizzy size="64" />}
+		content={<div>Brak definicji wartości określających badanie.</div>}
+	/>
+);
